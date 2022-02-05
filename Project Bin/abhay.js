350525679254b89be539f57ecb2e66a6e3a74bbe
Abhay Jha <95773361+dracs516@users.noreply.github.com>
2022-02-05 09:37:53 +0530

    Delete Project Bin directory

--- Project Bin/abhay.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-}
